99d72671b467d05435547d6e26b7ba0a8c1f7a2d
Rita Camacho <a104439@alunos.uminho.pt>
2024-04-28 19:03:16 +0100

    feat: distance and repeating & weighted activities

---
+++ src/main/java/com/group11/sportify/activities/distance/TrackAndField.java
@@ -0,0 +1,84 @@
+package com.group11.sportify.activities.distance;
+
+import com.group11.sportify.activities.Activity;
+import com.group11.sportify.users.User;
+
+import java.time.LocalDate;
+
+public class TrackAndField extends ActivityDistance {
+
+    /*
+     * Default constructor for the TrackAndField class.
+     */
+    public TrackAndField(){
+        super();
+    }
+
+    /**
+     * Constructor for the TrackAndField class.
+     * @param code The code of the exercise.
+     * @param description The description of the exercise.
+     * @param time The time spent performing the exercise in minutes.
+     * @param averageHeartRate The average heart rate during the activity.
+     * @param date The date of the activity.
+     * @param distance The distance covered during the activity.
+     * @param user the user associated with this activity.
+     */
+    public TrackAndField(int code, String description, int time, int averageHeartRate, LocalDate date, double distance, User user){
+        super(code, description, time, averageHeartRate, date, distance, user);
+    }
+
+    /**
+     * Constructor for the TrackAndField class.
+     * @param a The activity to copy.
+     * @param distance The distance covered during the activity.
+     */
+    public TrackAndField(Activity a, double distance){
+        super(a, distance);
+    }
+
+    /**
+     * Constructor for the TrackAndField class.
+     * @param c The activity to copy.
+     */
+    public TrackAndField(TrackAndField c){
+        super(c);
+    }
+
+    /**
+     * Clones the TrackAndField object.
+     * @return A new instance of TrackAndField object with the same attributes as the original.
+     */
+    public TrackAndField clone(){
+        return new TrackAndField(this);
+    }
+
+    /**
+     * Returns a string representation of the TrackAndField object.
+     * @return A string representation of the TrackAndField object.
+     */
+    public String toString(){
+        StringBuilder sb = new StringBuilder();
+
+        sb.append(super.toString());
+        sb.append("Track and Field\n");
+
+        return sb.toString();
+    }
+
+    /**
+     * Calculates the total calories burned during track And field based on the user's characteristics.
+     *
+     * @param user The user doing track and field.
+     * @return The total calories burned during track and field.
+     */
+    public double calculateCaloriesConsume(User user) {
+        double distance = this.getDistance();
+        double userFactor = user.calculateCaloriesFactor();
+        int heartRate = user.getAverageHeartRate() + (int) (2/userFactor)*25;
+        double energyExpenditurePerKm = 0.2;
+        this.setAverageHeartRateDuringActivity(heartRate);
+        double caloriesBurned = (1.1 * distance * energyExpenditurePerKm);
+        return caloriesBurned * userFactor;
+    }
+}
---
+++ src/main/java/com/group11/sportify/activities/repeating/weight/LegExtension.java
@@ -0,0 +1,98 @@
+package com.group11.sportify.activities.repeating.weight;
+
+import com.group11.sportify.activities.Activity;
+import com.group11.sportify.activities.repeating.ActivityRepetitions;
+import com.group11.sportify.users.User;
+
+import java.time.LocalDate;
+
+public class LegExtension extends ActivityRepetitionsWeight{
+
+    /*
+     * Default constructor for the LegExtension class.
+     */
+    public LegExtension(){
+        super();
+    }
+
+    /**
+     * Constructor for the LegExtension class.
+     * @param code The code of the exercise.
+     * @param description The description of the exercise.
+     * @param time The time spent performing the exercise in minutes.
+     * @param averageHeartRate The average heart rate during the activity.
+     * @param date The date of the activity.
+     * @param repetitions The number of repetitions of the activity.
+     * @param weight The weight used during the activity.
+     * @param user the user associated with this activity.
+     */
+    public LegExtension(int code, String description, int time, int averageHeartRate, LocalDate date, int repetitions, double weight, User user){
+        super(code, description, time, averageHeartRate, date, repetitions, weight, user);
+    }
+
+    /**
+     * Constructor for the LegExtension class.
+     * @param a The activity to copy.
+     * @param repetitions The number of repetitions of the activity.
+     * @param weight The weight used during the activity.
+     */
+    public LegExtension(Activity a, int repetitions, double weight){
+        super(a, repetitions, weight);
+    }
+
+    /**
+     * Constructor for the LegExtension class.
+     * @param a The activity to copy.
+     * @param weight The weight used during the activity.
+     */
+    public LegExtension(ActivityRepetitions a, double weight){
+        super(a, weight);
+    }
+
+    /**
+     * Constructor for the LegExtension class.
+     * @param c The activity to copy.
+     */
+    public LegExtension(LegExtension c){
+        super(c);
+    }
+
+    /**
+     * Clones the LegExtension object.
+     * @return A new instance of LegExtension object with the same attributes as the original.
+     */
+    public LegExtension clone(){
+        return new LegExtension(this);
+    }
+
+    /**
+     * Returns a string representation of the LegExtension object.
+     * @return A string representation of the LegExtension object.
+     */
+    public String toString(){
+        StringBuilder sb = new StringBuilder();
+
+        sb.append(super.toString());
+        sb.append("Leg Extension\n");
+
+        return sb.toString();
+    }
+
+    /**
+     * Calculates the total calories burned during leg extensions based on the user's characteristics.
+     *
+     * @param user The user doing leg extensions.
+     * @return The total calories burned during leg extensions.
+     */
+    public double calculateCaloriesConsume(User user) {
+        int reps = this.getRepetitions();
+        double weight = this.getWeight();
+        double userFactor = user.calculateCaloriesFactor();
+        int heartRate = user.getAverageHeartRate() + (int) (1/userFactor)*50;
+        double energyExpenditurePerRep = 0.1;
+        double energyExpenditureForWeight = 0.25;
+        this.setAverageHeartRateDuringActivity(heartRate);
+        double caloriesBurned = (reps * energyExpenditurePerRep) + (weight * energyExpenditureForWeight);
+        return caloriesBurned * userFactor;
+    }
+}
